f2334a0aa095f04006dd0ee15e1b14c25c0e7e8d
Vipplay <vitaliy.yanitskyy@gmail.com>
2024-05-24 10:44:15 +0300

    Done

--- module3/Program.cs
+++ module3/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System.Xml.Serialization;
+
+namespace module3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Employees employees;
+            XmlSerializer xmlSerializer = new(typeof(Employees));
+
+            using (FileStream fs = new("employees.xml", FileMode.Open))
+            {
+                employees = (Employees)xmlSerializer.Deserialize(fs);
+            }
+
+            var sortedEmployees = employees.EmployeeList.OrderBy(e => e.HireDate);
+
+            using (StreamWriter sw = new("employees.txt"))
+            {
+                foreach (var employee in sortedEmployees)
+                {
+                    sw.WriteLine($"Name: {employee.Name} Position: {employee.Position} HireDate: {employee.HireDate:yyyy-MM-dd}");
+                }
+            }
+
+            using (FileStream fs = new("sorted_employees.xml", FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, new Employees { EmployeeList = sortedEmployees.ToList() });
+            }
+        }
+    }
+
+    [XmlRoot("Employee")]
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+
+    [XmlRoot("Employees", Namespace = "")]
+    public class Employees
+    {
+        [XmlElement("Employee")]
+        public List<Employee> EmployeeList { get; set; }
+    }
+}
--- module3/obj/Debug/net8.0/module3.AssemblyInfo.cs
+++ module3/obj/Debug/net8.0/module3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("module3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07080bfe58edba4be6aecf8b9d17e9155efb282d")]
 [assembly: System.Reflection.AssemblyProductAttribute("module3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("module3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
